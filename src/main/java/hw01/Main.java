f88a091eaf165f4d7b249f4639e4091fcc186233
Chang Min Bark <cb073@bucknell.edu>
2023-10-11 10:27:15 -0400

    main class for hw01

---
+++ src/main/java/hw01/Main.java
@@ -0,0 +1,30 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Fall 2023
+ * Instructor: Prof. Joshua Stough
+ *
+ * Name: Chang Min Bark
+ * Section: 02
+ * Date: 10/11/2023
+ * Time: 10:26 AM
+ *
+ * Project: csci205_hw
+ * Package: hw01
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package hw01;
+
+public class Main {
+
+    /**
+     * The main function to be run
+     * @param args default main parameters
+     */
+    public static void main(String[] args) {
+
+    }
+}
